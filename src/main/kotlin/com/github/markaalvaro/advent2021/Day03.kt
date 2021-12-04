7f51d0c8ff88f4e436f7f952fa636336dab85f8e
Mark Alvaro <markaalvaro@gmail.com>
2021-12-04 10:42:56 -0500

    Complete day 4 puzzle and rename test file.

--- src/main/kotlin/com/github/markaalvaro/advent2021/Day03.kt
+++ src/main/kotlin/com/github/markaalvaro/advent2021/Day03.kt
@@ -2,9 +2,8 @@ package com.github.markaalvaro.advent2021
 
 private const val FILE_NAME = "Day03.txt"
 
-fun String.binaryToInt(): Int {
-    return Integer.parseInt(this, 2)
-}
+val String.binaryToInt: Int
+    get() { return Integer.parseInt(this, 2) }
 
 fun List<String>.mostCommonBit(index: Int): Char {
     val count1 = this.count { it[index] == '1' }
@@ -28,29 +27,29 @@ fun binaryDiagnostic1(): Int {
 
     val epsilon = gamma.toList().map { if (it == '1') '0' else '1' }.joinToString("")
 
-    return gamma.binaryToInt() * epsilon.binaryToInt()
+    return gamma.binaryToInt * epsilon.binaryToInt
 }
 
 fun binaryDiagnostic2(): Int {
-    var oxygen = readFile(FILE_NAME)
-    var cO2 = oxygen.toList()
+    var o2 = readFile(FILE_NAME)
+    var cO2 = o2.toList()
 
     var i = 0
-    while (oxygen.size > 1 || cO2.size > 1) {
-        if (oxygen.size > 1) {
-            val mostCommonOxygen = oxygen.mostCommonBit(i)
-            oxygen = oxygen.filter { it[i] == mostCommonOxygen }
+    while (o2.size > 1 || cO2.size > 1) {
+        if (o2.size > 1) {
+            val mostCommon = o2.mostCommonBit(i)
+            o2 = o2.filter { it[i] == mostCommon }
         }
 
         if (cO2.size > 1) {
-            val mostCommonCO2 = cO2.mostCommonBit(i)
-            cO2 = cO2.filter { it[i] != mostCommonCO2 }
+            val mostCommon = cO2.mostCommonBit(i)
+            cO2 = cO2.filter { it[i] != mostCommon }
         }
 
         i++
     }
 
-    return oxygen[0].binaryToInt() * cO2[0].binaryToInt()
+    return o2[0].binaryToInt * cO2[0].binaryToInt
 }
 
 fun main() {
---
+++ src/main/kotlin/com/github/markaalvaro/advent2021/Day04.kt
@@ -0,0 +1,76 @@
+package com.github.markaalvaro.advent2021
+
+private const val FILE_NAME = "Day04.txt"
+
+fun giantSquid1(): Int {
+    val input = readFile(FILE_NAME)
+    val numbers = getNumbers(input)
+    val boards = getBoards(input)
+
+    val numsSoFar = mutableListOf<Int>()
+    for (num in numbers) {
+        numsSoFar.add(num)
+        boards.firstOrNull { isBingo(it, numsSoFar) }?.let { return calculateScore(it, numsSoFar) }
+    }
+
+    throw IllegalStateException("Expected at least one winning bingo")
+}
+
+fun giantSquid2(): Int {
+    val input = readFile(FILE_NAME)
+    val numbers = getNumbers(input)
+    var boards = getBoards(input)
+
+    val numsSoFar = mutableListOf<Int>()
+    for (num in numbers) {
+        numsSoFar.add(num)
+        if (boards.size == 1) {
+            if (isBingo(boards[0], numsSoFar))
+                return calculateScore(boards[0], numsSoFar)
+        }
+        else
+            boards = boards.filter { !isBingo(it, numsSoFar) }.toMutableList()
+    }
+
+    throw IllegalStateException("Expected a single last board to win")
+}
+
+fun main() {
+    println(giantSquid2())
+    println(giantSquid2())
+}
+
+private fun isBingo(board: List<Int>, numbers: List<Int>): Boolean {
+    for (i in 0..4) {
+        // Check rows
+        val start = i * 5
+        val row = listOf(board[start], board[start + 1], board[start + 2], board[start + 3], board[start + 4])
+        if (numbers.containsAll(row)) return true
+
+        // Check columns
+        val column = listOf(board[i], board[i + 5], board[i + 10], board[i + 15], board[i + 20])
+        if (numbers.containsAll(column)) return true
+    }
+    return false
+}
+
+private fun getNumbers(input: List<String>): List<Int> {
+    return input[0].split(",").map { it.toInt() }
+}
+
+private fun getBoards(input: List<String>): MutableList<List<Int>> {
+    return input.drop(2)
+        .chunked(6)
+        .map { boardLines ->
+            boardLines.take(5)
+                .flatMap { it.split(",") }
+                .flatMap { it.split(" ") }
+                .filter { it != "" }
+                .map { it.toInt() }
+        }
+        .toMutableList()
+}
+
+private fun calculateScore(board: List<Int>, numsSoFar: List<Int>): Int {
+    return (board - numsSoFar.toSet()).sum() * numsSoFar.last()
+}
