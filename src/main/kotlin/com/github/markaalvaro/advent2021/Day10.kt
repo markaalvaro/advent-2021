f1a062a5e19c6f9b85278c0590269983e3c0296e
Mark Alvaro <markaalvaro@gmail.com>
2021-12-10 09:58:22 -0500

    Fix formatting in day 10.

--- src/main/kotlin/com/github/markaalvaro/advent2021/Day10.kt
+++ src/main/kotlin/com/github/markaalvaro/advent2021/Day10.kt
@@ -9,15 +9,15 @@ val openToClose = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
 val closeToScore1 = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
 val closeToScore2 = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
 
-fun syntaxScoring1() : Int {
-    val nums : List<Int> = readFile(FILE_NAME)
+fun syntaxScoring1(): Int {
+    val nums: List<Int> = readFile(FILE_NAME)
         .map {
             val stack = Stack<Char>()
             for (bracket in it.toList()) {
                 if (bracket in openBrackets) stack.push(openToClose[bracket])
                 else {
                     if (stack.peek() == bracket) stack.pop()
-                    else { return@map closeToScore1[bracket]!! }
+                    else return@map closeToScore1[bracket]!!
                 }
             }
             0
@@ -26,7 +26,7 @@ fun syntaxScoring1() : Int {
     return nums.sum()
 }
 
-fun syntaxScoring2() : Long {
+fun syntaxScoring2(): Long {
     val scores = readFile(FILE_NAME)
         .map {
             val stack = Stack<Char>()
@@ -34,7 +34,7 @@ fun syntaxScoring2() : Long {
                 if (bracket in openBrackets) stack.push(openToClose[bracket])
                 else {
                     if (stack.peek() == bracket) stack.pop()
-                    else { return@map 0 }
+                    else return@map 0
                 }
             }
 
