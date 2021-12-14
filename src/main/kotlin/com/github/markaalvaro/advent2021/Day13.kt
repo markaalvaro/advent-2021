866a90e48f0c1712f4df23fe3b29bc99df5fce89
Mark Alvaro <markaalvaro@gmail.com>
2021-12-13 19:29:03 -0500

    Minor cleanup to make solution work for any input.

--- src/main/kotlin/com/github/markaalvaro/advent2021/Day13.kt
+++ src/main/kotlin/com/github/markaalvaro/advent2021/Day13.kt
@@ -4,8 +4,15 @@ private const val FILE_NAME = "Day13.txt"
 
 fun transparentOragami1(): Int {
     val input = readFile(FILE_NAME)
-    val grid = createGrid(input)
-    return grid.xFold(655).flatMap { it.toList() }.sumOf { it }
+    var grid = createGrid(input)
+
+    input.first { it.startsWith("fold") }
+        .let {
+            val (instruction, value) = it.split("=")
+            grid = if (instruction.endsWith("x")) grid.xFold(value.toInt()) else grid.yFold(value.toInt())
+        }
+
+    return grid.flatMap { it.toList() }.sumOf { it }
 }
 
 fun transparentOragami2(): Int {
@@ -33,9 +40,8 @@ private fun createGrid(input: List<String>): Array<IntArray> {
         .map { it.split(",") }
         .map { (first, second) -> Pair(second.toInt(), first.toInt()) } // reverse to use sane indexing
         .toSet()
-
-    val rows = coords.maxOf { it.first } + 2 // Need to make it odd for some reason
-    val cols = coords.maxOf { it.second } + 1
+    var rows = coords.maxOf { it.first } + 1; if (rows % 2 == 0) rows++
+    var cols = coords.maxOf { it.second } + 1; if (cols % 2 == 0) cols++
     return Array(rows) { r -> IntArray(cols) { c -> if (Pair(r, c) in coords) 1 else 0 } }
 }
 
