87fab9b781f53964ac4320a56cfde20c19039197
Mark Alvaro <markaalvaro@gmail.com>
2021-12-13 19:15:00 -0500

    Complete day 13 puzzles.

---
+++ src/main/kotlin/com/github/markaalvaro/advent2021/Day13.kt
@@ -0,0 +1,66 @@
+package com.github.markaalvaro.advent2021
+
+private const val FILE_NAME = "Day13.txt"
+
+fun transparentOragami1(): Int {
+    val input = readFile(FILE_NAME)
+    val grid = createGrid(input)
+    return grid.xFold(655).flatMap { it.toList() }.sumOf { it }
+}
+
+fun transparentOragami2(): Int {
+    val input = readFile(FILE_NAME)
+    var grid = createGrid(input)
+
+    input.dropWhile { !it.startsWith("fold") }
+        .map { it.split("=") }
+        .forEach { (instruction, value) ->
+            grid = if (instruction.endsWith("x")) grid.xFold(value.toInt()) else grid.yFold(value.toInt())
+        }
+
+    grid.prettyPrint()
+
+    return grid.flatMap { it.toList() }.sumOf { it }
+}
+
+fun main() {
+    println(transparentOragami1())
+    println(transparentOragami2())
+}
+
+private fun createGrid(input: List<String>): Array<IntArray> {
+    val coords = input.takeWhile { it.isNotBlank() }
+        .map { it.split(",") }
+        .map { (first, second) -> Pair(second.toInt(), first.toInt()) } // reverse to use sane indexing
+        .toSet()
+
+    val rows = coords.maxOf { it.first } + 2 // Need to make it odd for some reason
+    val cols = coords.maxOf { it.second } + 1
+    return Array(rows) { r -> IntArray(cols) { c -> if (Pair(r, c) in coords) 1 else 0 } }
+}
+
+private fun Array<IntArray>.xFold(value: Int): Array<IntArray> {
+    val result = map { it.take(value).toIntArray() }
+        .toTypedArray()
+    for (r in 0..result.lastIndex)
+        for (c in 0..result[r].lastIndex)
+            result[r][c] = result[r][c] or this[r][this[r].lastIndex - c]
+    return result
+}
+
+private fun Array<IntArray>.yFold(value: Int): Array<IntArray> {
+    val result = take(value).toTypedArray()
+    for (r in 0..result.lastIndex)
+        for (c in 0..result[r].lastIndex)
+                result[r][c] = result[r][c] or this[this.lastIndex - r][c]
+    return result
+}
+
+private fun Array<IntArray>.prettyPrint() {
+    for (r in 0..lastIndex) {
+        for (c in 0..this[r].lastIndex) {
+            print("${if (this[r][c] == 1) '#' else ' '}")
+        }
+        println()
+    }
+}
